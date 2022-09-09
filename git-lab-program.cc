a9e5d61226fe94a8ee1b233f3ad32d0d9a85051f
addie1999 <112737772+addie1999@users.noreply.github.com>
2022-09-09 16:42:18 -0400

    created git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+    File: git-lab-program.cc
+    Author: Addie Duffy
+    Date: Fri Sept 9th 2022
+    Description: file for git-lab-2
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
+
+
